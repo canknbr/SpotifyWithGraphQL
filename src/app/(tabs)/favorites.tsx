89831c3f73d5a3e1d61167e6c1a0b2106ef12d6c
canknbr <cankanbur1@gmail.com>
2023-10-13 09:00:13 +0300

    added bottom tab

---
+++ src/app/(tabs)/favorites.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const FavoriesScreen = () => {
+  return (
+    <View>
+      <Text>FavoriesScreen</Text>
+    </View>
+  );
+};
+
+export default FavoriesScreen;
