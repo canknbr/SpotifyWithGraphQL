e3918369916ea32f20701835d9952e324ae15c48
canknbr <cankanbur1@gmail.com>
2023-10-13 10:12:35 +0300

    added player logic

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,15 +1,67 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
+import { AVPlaybackStatus, Audio } from "expo-av";
+import { Sound } from "expo-av/build/Audio";
 
 import { usePlayerContext } from "../providers/PlayerProvider";
+import { useState, useEffect } from "react";
 
 const Player = () => {
+  const [sound, setSound] = useState<Sound>();
+  const [isPlaying, setIsPlaying] = useState(false);
   const { track } = usePlayerContext();
+
+  useEffect(() => {
+    playTrack();
+  }, [track]);
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const playTrack = async () => {
+    if (sound) {
+      await sound.unloadAsync();
+    }
+
+    if (!track?.preview_url) {
+      return;
+    }
+    const { sound: newSound } = await Audio.Sound.createAsync({
+      uri: track.preview_url,
+    });
+
+    setSound(newSound);
+    newSound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+    await newSound.playAsync();
+  };
+  const onPlaybackStatusUpdate = (status: AVPlaybackStatus) => {
+    if (!status.isLoaded) {
+      return;
+    }
+
+    setIsPlaying(status.isPlaying);
+  };
+
+  const onPlayPause = async () => {
+    if (!sound) {
+      return;
+    }
+    if (isPlaying) {
+      await sound.pauseAsync();
+    } else {
+      await sound.playAsync();
+    }
+  };
+
   if (!track) {
     return null;
   }
-
-  const image = track.album.images?.[0];
+  const image = track?.album.images?.[0];
 
   return (
     <View style={styles.container}>
@@ -17,19 +69,21 @@ const Player = () => {
         {image && <Image source={{ uri: image.url }} style={styles.image} />}
 
         <View style={{ flex: 1 }}>
-          <Text style={styles.title}>{track.name}</Text>
-          <Text style={styles.subtitle}>{track.artists[0]?.name}</Text>
+          <Text style={styles.title}>{track?.name}</Text>
+          <Text style={styles.subtitle}>{track?.artists[0]?.name}</Text>
         </View>
 
         <Ionicons
+          disabled={!track?.preview_url}
           name={"heart-outline"}
           size={20}
           color={"white"}
           style={{ marginHorizontal: 10 }}
         />
         <Ionicons
+          onPress={onPlayPause}
           disabled={!track?.preview_url}
-          name={"play"}
+          name={isPlaying ? "pause" : "play"}
           size={22}
           color={track?.preview_url ? "white" : "gray"}
         />
